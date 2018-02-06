01a037a8e4fb145cc2a1b1bcad6be49eb6ff05fd
jeongjoonsup <penny4860@gmail.com>
2018-02-06 20:11:36 +0900

    test_for_kitti_dataset off

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ def run():
     image_shape = (160, 576)
     data_dir = './data'
     runs_dir = './runs'
-    tests.test_for_kitti_dataset(data_dir)
+    # tests.test_for_kitti_dataset(data_dir)
 
     # Download pretrained vgg model
     helper.maybe_download_pretrained_vgg(data_dir)
