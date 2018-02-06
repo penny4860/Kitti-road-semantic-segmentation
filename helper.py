983655c6674ab22318e79a6a8c83b1f349b17dad
jeongjoonsup <penny4860@gmail.com>
2018-02-06 20:44:05 +0900

    training 뒤에 train sample을 inference하는 것으로 수정

--- helper.py
+++ helper.py
@@ -135,6 +135,6 @@ def save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_p
     # Run NN on test images and save them to HD
     print('Training Finished. Saving test images to: {}'.format(output_dir))
     image_outputs = gen_test_output(
-        sess, logits, keep_prob, input_image, os.path.join(data_dir, 'data_road/testing'), image_shape)
+        sess, logits, keep_prob, input_image, os.path.join(data_dir, 'data_road/training'), image_shape)
     for name, image in image_outputs:
         scipy.misc.imsave(os.path.join(output_dir, name), image)
