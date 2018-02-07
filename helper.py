4fa80a0330e3a62007d95d3a59d5e45ff7bdd5a0
jeongjoonsup <penny4860@gmail.com>
2018-02-07 22:23:16 +0900

    batch module created

--- helper.py
+++ helper.py
@@ -1,5 +1,3 @@
-import re
-import random
 import numpy as np
 import os.path
 import scipy.misc
@@ -56,46 +54,6 @@ def maybe_download_pretrained_vgg(data_dir):
 
         # Remove zip file to save space
         os.remove(os.path.join(vgg_path, vgg_filename))
-
-
-def gen_batch_function(data_folder, image_shape):
-    """
-    Generate function to create batches of training data
-    :param data_folder: Path to folder that contains all the datasets
-    :param image_shape: Tuple - Shape of image
-    :return:
-    """
-    def get_batches_fn(batch_size):
-        """
-        Create batches of training data
-        :param batch_size: Batch Size
-        :return: Batches of training data
-        """
-        image_paths = glob(os.path.join(data_folder, 'image_2', '*.png'))
-        label_paths = {
-            re.sub(r'_(lane|road)_', '_', os.path.basename(path)): path
-            for path in glob(os.path.join(data_folder, 'gt_image_2', '*_road_*.png'))}
-        background_color = np.array([255, 0, 0])
-
-        random.shuffle(image_paths)
-        for batch_i in range(0, len(image_paths), batch_size):
-            images = []
-            gt_images = []
-            for image_file in image_paths[batch_i:batch_i+batch_size]:
-                gt_image_file = label_paths[os.path.basename(image_file)]
-
-                image = scipy.misc.imresize(scipy.misc.imread(image_file), image_shape)
-                gt_image = scipy.misc.imresize(scipy.misc.imread(gt_image_file), image_shape)
-
-                gt_bg = np.all(gt_image == background_color, axis=2)
-                gt_bg = gt_bg.reshape(*gt_bg.shape, 1)
-                gt_image = np.concatenate((gt_bg, np.invert(gt_bg)), axis=2)
-
-                images.append(image)
-                gt_images.append(gt_image)
-
-            yield np.array(images), np.array(gt_images)
-    return get_batches_fn
 
 
 def gen_test_output(sess, logits, keep_prob, image_pl, data_folder, image_shape):
