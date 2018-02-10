506359abde0747901dd8972409961fc35215aa6e
jeongjoonsup <penny4860@gmail.com>
2018-02-10 19:22:06 +0900

    useless code deleted

--- inference.py
+++ inference.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import tensorflow as tf
-import numpy as np
-import os
 import helper
 
-from src.utils import plot_img
 from src.fcn import FcnModel
-from src.batch import gen_batch_function
 
 if __name__ == '__main__':
     num_classes = 2
@@ -19,9 +15,7 @@ if __name__ == '__main__':
     y_placeholder = tf.placeholder(tf.float32, [None, image_shape[0], image_shape[1], num_classes])
     lr_placeholder = tf.placeholder(tf.float32)
     is_train_placeholder = tf.placeholder(tf.bool)
-
     fcn_model = FcnModel(x_placeholder, y_placeholder, is_train_placeholder, num_classes)
-    get_batches_fn = gen_batch_function(os.path.join(data_dir, 'data_road/training'), image_shape)
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
