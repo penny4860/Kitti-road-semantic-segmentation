803e3562b693d661650db1ee553d0f70a8d15010
jeongjoonsup <penny4860@gmail.com>
2018-02-10 19:21:29 +0900

    inference driver 정리

--- inference.py
+++ inference.py
@@ -3,6 +3,7 @@
 import tensorflow as tf
 import numpy as np
 import os
+import helper
 
 from src.utils import plot_img
 from src.fcn import FcnModel
@@ -23,22 +24,9 @@ if __name__ == '__main__':
     get_batches_fn = gen_batch_function(os.path.join(data_dir, 'data_road/training'), image_shape)
 
     with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
         saver = tf.train.Saver()
-        saver.save(sess, "models/model.ckpt")
         saver.restore(sess, "models/model.ckpt")
         
-        for images, labels in get_batches_fn(1):
-            y_pred = sess.run(tf.nn.softmax(fcn_model.inference_op), feed_dict = {x_placeholder: images,
-                                                                                  is_train_placeholder : False})
-            sess.run(fcn_model.update_op, feed_dict = {x_placeholder: images,
-                                                       y_placeholder: np.array(labels),
-                                                       is_train_placeholder : False})
-            accuracy, iou = sess.run([fcn_model.accuracy_op, fcn_model.iou_op], feed_dict = {x_placeholder: images,
-                                                                                             y_placeholder: np.array(labels),
-                                                                                             is_train_placeholder : False})
-            print(accuracy, iou)
-            plot_img([images[0], labels[0, :, :, 1], y_pred[0, :, :, 1]])
-            break
-            
-            
-            
+        logits = tf.reshape(fcn_model.inference_op, (-1, num_classes))
+        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, is_train_placeholder, x_placeholder)
