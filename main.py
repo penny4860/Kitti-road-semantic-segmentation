05f5977d9df82b2e443a46476e5f9778c337dcbc
jeongjoonsup <penny4860@gmail.com>
2018-02-10 17:03:51 +0900

    nit : plot type

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ def plot_img(images, show=True, save_filename=None):
     fig, ax = plt.subplots(nrows=1, ncols=len(images))
     for i, img in enumerate(images):
         plt.subplot(4, 1, i+1)
-        plt.imshow(img)
+        plt.imshow(img, cmap="gray")
     if show:
         plt.show()
     if save_filename:
